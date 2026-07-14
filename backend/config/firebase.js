b28566ae3d110f84dbf6cf2a24db8d8decaee669
ayush-godhani <ayushgodhani456@gmail.com>
2026-07-14 18:04:28 +0530

    Use Firebase service account from environment

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: serviceAccount.project_id + ".appspot.com",
+    storageBucket: `${serviceAccount.project_id}.appspot.com`,
   });
 }
 
